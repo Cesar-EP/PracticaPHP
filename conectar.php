0efc8fe22d28054a741111ea22ae0f0a71748ec5
raonnyg23 <raonnyg234@gmail.com>
2024-04-26 11:55:32 -0400

    ahi esta las tablas

---
+++ conectar.php
@@ -0,0 +1,13 @@
+<?php
+$mysqlli = new mysqli("localhost", "root", "","tablasp");
+
+
+if ($mysqlli->connect_errno){
+
+echo "fallo a connectar a MySQL: (" .  i->connect_errno . ") " . $mysqli -> connect_error; 
+
+
+
+}
+
+?>
